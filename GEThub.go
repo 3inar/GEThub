9c53552861a7b8e9355e1f3734a3d95864932b2d
Einar Holsbø <einar@cs.uit.no>
2014-10-02 18:54:41 +0200

    this is perfect

---
+++ GEThub.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+import "github.com/google/go-github/github"
+import "code.google.com/p/goauth2/oauth"
+
+func main() {
+    // NB: go to github and make yourself an access token
+    token := "your token goes here"
+    t := &oauth.Transport{
+        Token: &oauth.Token{AccessToken: token},
+    }
+
+    client := github.NewClient(t.Client())
+    repos, _, _ := client.Repositories.ListForks("uit-inf-3200", "Project-1",
+                                                 nil)
+    messages := make([]string, 0)
+
+    for _, fork := range repos {
+        comms, _, _ := client.Repositories.ListCommits(*fork.Owner.Login,
+            *fork.Name, nil)
+        for _, commit := range comms{
+            messages= append(messages, *commit.Commit.Message)
+        }
+    }
+
+    for _, m := range messages { fmt.Println(m) } 
+}
