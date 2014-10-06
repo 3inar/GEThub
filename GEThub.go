bce3c9920b01d5f70a777326e633e60ef5cb1a3f
Bjørn Fjukstad <bjorn@cs.uit.no>
2014-10-06 14:47:42 +0200

    config file for app token
    Add your github app token in the `conf.json` file. pow!

--- GEThub.go
+++ GEThub.go
@@ -1,15 +1,52 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"flag"
 	"fmt"
+	"os"
 
 	"code.google.com/p/goauth2/oauth"
 	"github.com/google/go-github/github"
 )
 
+type Config struct {
+	Token string
+}
+
+func getAppToken(filename string) (string, error) {
+
+	file, err := os.Open(filename)
+
+	if err != nil {
+		return "", errors.New("Cannot find config file!")
+	}
+
+	decoder := json.NewDecoder(file)
+	config := Config{}
+
+	err = decoder.Decode(&config)
+
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+
+	return config.Token, nil
+}
+
 func main() {
-	// NB: go to github and make yourself an access token
-	token := "your token goes here"
+	configFile := flag.String("configfile", "conf.json",
+		"Configuration file. Where your app token lives")
+
+	flag.Parse()
+
+	token, err := getAppToken(*configFile)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	t := &oauth.Transport{
 		Token: &oauth.Token{AccessToken: token},
 	}
